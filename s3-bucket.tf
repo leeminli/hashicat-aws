258e815303db48acda5d0f8bfe54cb29c13f217f
Reanne Lee <leeml@pcs-security.com>
2023-09-04 09:06:30 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,8 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "reanne-lee"
   acl    = "private"
-  bucket_prefix = "reanne-lee"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
